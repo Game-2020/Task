7913e0c431d440f4430385c6d5c279969803697f
Game-2020 <78715190+Game-2020@users.noreply.github.com>
2023-01-13 01:15:42 +0800

    Update if.js

--- if.js
+++ if.js
@@ -1,8 +1,9 @@
-/*******************************
-hostname = app.ilovechina.tech
-
+/*
 ^https:\/\/app\.ilovechina\.tech\/api\.php? url script-response-body https://raw.githubusercontent.com/Game-2020/Task/main/if.js
-*******************************/
+
+MITM = app.ilovechina.tech
+*/
+
 var body=$response.body;
 body = body.replace(/"endtime\":"2023-01-14"/g,'"endtime":"2222-12-12"');
 body = body.replace(/"state\":"1"/g,'"state":"1"');
